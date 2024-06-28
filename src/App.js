81e07080a9319711762308696e31f50b15f4c7ed
marcilioreis <emaildomarcilio@gmail.com>
2024-06-28 16:16:52 -0300

    [CHORE]: Deploy

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React from 'react'
 import Measure from './components/Measure'
 
 function App() {
-  return (
-    <div id="canvas-container">
-      <Measure></Measure>
-      <p>Teste</p>
-    </div>
-  );
+    return (
+        <div id="canvas-container">
+            <Measure></Measure>
+        </div>
+    )
 }
 
-export default App;
-
+export default App
